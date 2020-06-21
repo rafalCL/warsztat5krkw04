2e0ec5cf60f9376205c9ec26f6da86621baea732
Rafal <rafal.opial@coderslab.pl>
2020-06-21 15:00:10 +0200

    Serve all books

--- src/main/java/pl/coderslab/warsztat5krkw04/controller/BookController.java
+++ src/main/java/pl/coderslab/warsztat5krkw04/controller/BookController.java
@@ -4,13 +4,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.warsztat5krkw04.model.Book;
+import pl.coderslab.warsztat5krkw04.service.MemoryBookService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    private MemoryBookService mbs;
+
+    public BookController(MemoryBookService mbs) {
+        this.mbs = mbs;
+    }
+
     @GetMapping("/helloBook")
     public Book helloBook(){
         return new Book(1L,"9788324631766","Thinking in Java",
                 "Bruce Eckel","Helion","programming");
+    }
+
+    @GetMapping("")
+    public List<Book> getAll(){
+        return this.mbs.getList();
     }
 }
