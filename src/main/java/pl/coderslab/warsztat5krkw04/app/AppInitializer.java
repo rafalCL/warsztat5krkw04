f93cdd394664c9be7b419a4d1148dc617afd164d
Rafal <rafal.opial@coderslab.pl>
2020-06-21 12:35:01 +0200

    Initialize maven mvc project

---
+++ src/main/java/pl/coderslab/warsztat5krkw04/app/AppInitializer.java
@@ -0,0 +1,15 @@
+package pl.coderslab.warsztat5krkw04.app;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() { return null; }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class}; }
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"}; }
+}
---
+++ src/main/java/pl/coderslab/warsztat5krkw04/controller/HomeController.java
@@ -0,0 +1,13 @@
+package pl.coderslab.warsztat5krkw04.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("")
+    public String home(){
+        return "{status: running}";
+    }
+}
